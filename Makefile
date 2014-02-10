2fa9f8e1514676eb9302272d8152781079191be7
Pablo Neira Ayuso <pablo@gnumonks.org>
2014-02-10 17:09:05 +0100

    gtp-tunnel-add: add gtp_build_payload
    Simple refactoring to prepare the tunnel deletion operation.

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ default:
 	$(MAKE) -C $(KDIR) SUBDIRS=$(PWD) modules
 	$(CC) -lmnl gtp-link-add.c -o gtp-link-add
 	$(CC) -lmnl gtp-cfg-add.c genl.c -o gtp-cfg-add
-	$(CC) -lmnl gtp-tunnel-add.c genl.c -o gtp-tunnel-add
+	$(CC) -lmnl gtp-tunnel-add.c genl.c gtpnl.c -o gtp-tunnel-add
 	$(CC) -lmnl -g gtp-tunnel-get.c genl.o -o gtp-tunnel-get
 clean:
 	rm -rf *.o *.mod.* modules.order Module.symvers *.ko .tmp_versions .*.cmd
--- gtp-tunnel-add.c
+++ gtp-tunnel-add.c
@@ -14,23 +14,6 @@
 
 static uint32_t seq;
 
-static struct nlmsghdr *build_msg(int type, char *buf, int i, uint32_t ifidx,
-				  uint32_t sgsn_addr, uint32_t ms_addr)
-{
-	struct nlmsghdr *nlh;
-
-	nlh = genl_nlmsg_build_hdr(buf, type, NLM_F_EXCL | NLM_F_ACK, ++seq,
-				   GTP_CMD_TUNNEL_NEW);
-
-	mnl_attr_put_u32(nlh, GTPA_VERSION, GTP_V0);
-	mnl_attr_put_u32(nlh, GTPA_LINK, ifidx);
-	mnl_attr_put_u32(nlh, GTPA_SGSN_ADDRESS, sgsn_addr);
-	mnl_attr_put_u32(nlh, GTPA_MS_ADDRESS, ms_addr);
-	mnl_attr_put_u64(nlh, GTPA_TID, i);
-
-	return nlh;
-}
-
 int main(int argc, char *argv[])
 {
 	struct mnl_socket *nl;
@@ -42,8 +25,8 @@ int main(int argc, char *argv[])
 	int32_t genl_id;
 	struct in_addr ms, sgsn;
 
-	if (argc != 5) {
-		printf("%s <gtp device> <tid> <ms-addr> <sgsn-addr>\n",
+	if (argc != 6) {
+		printf("%s <gtp device> <version> <tid> <ms-addr> <sgsn-addr>\n",
 			argv[0]);
 		exit(EXIT_FAILURE);
 	}
@@ -61,18 +44,20 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	if (inet_aton(argv[3], &ms) < 0) {
+	if (inet_aton(argv[4], &ms) < 0) {
 		perror("bad address for ms");
 		exit(EXIT_FAILURE);
 	}
 
-	if (inet_aton(argv[4], &sgsn) < 0) {
+	if (inet_aton(argv[5], &sgsn) < 0) {
 		perror("bad address for sgsn");
 		exit(EXIT_FAILURE);
 	}
 
-	nlh = build_msg(genl_id, buf, atoi(argv[2]), gtp_ifidx, sgsn.s_addr,
-			ms.s_addr);
+	nlh = genl_nlmsg_build_hdr(buf, genl_id, NLM_F_EXCL | NLM_F_ACK, ++seq,
+				   GTP_CMD_TUNNEL_NEW);
+	gtp_build_payload(nlh, atoi(argv[3]), gtp_ifidx, sgsn.s_addr,
+			  ms.s_addr, atoi(argv[2]));
 
 	if (genl_socket_talk(nl, nlh, seq, NULL, NULL) < 0)
 		perror("genl_socket_talk");
