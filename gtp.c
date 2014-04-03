fba534b95a2f71130174f0cbe1302eac57112bcc
Harald Welte <laforge@gnumonks.org>
2014-04-03 09:37:38 -0400

    Add copright headers and license files, list Pablo as author
    The kernel module still only listed me as author, even though Pablo
    was doing most of the work on it after my initial incomplete version.

--- gtp.c
+++ gtp.c
@@ -2,6 +2,7 @@
 
 /* (C) 2012-2014 by sysmocom - s.f.m.c. GmbH
  * Author: Harald Welte <hwelte@sysmocom.de>
+ * 	   Pablo Neira Ayuso <pablo@gnumonks.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
